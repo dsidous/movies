a6ae735abdf16622fa11adb4bf0ebc446811fa39
Tamas <jonastamas2@gmail.com>
2020-05-29 18:45:24 +0100

    feat: added LongCopy component

---
+++ packages/ui/src/components/atoms/LongCopy/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { compiler } from 'markdown-to-jsx';
+
+import { Button } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const LongCopy = ({ content, max = 500 }) => {
+  const classes = useStyles();
+  const [state, setState] = useState({
+    expanded: true,
+    longCopy: content,
+  });
+
+  const toggleContent = () => {
+    const longCopy = state.expanded
+      ? `${content.slice(0, content.indexOf(' ', max))} ...`
+      : content;
+
+    setState({
+      longCopy,
+      expanded: !state.expanded,
+    });
+  };
+
+  useEffect(() => {
+    if (content.length > max) {
+      toggleContent();
+    }
+  }, []);
+
+  return (
+    <>
+      {compiler(state.longCopy)}
+      {content.length > max && (
+        <Button
+          onClick={() => toggleContent()}
+          size="small"
+          variant="outlined"
+          className={classes.toggle}
+        >
+          {state.expanded ? <span>Show Less</span> : <span>Show More</span>}
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default LongCopy;
---
+++ packages/ui/src/components/atoms/LongCopy/styles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  toggle: {
+    alignSelf: 'flex-end',
+  },
+});
