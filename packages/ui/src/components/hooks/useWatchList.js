911e9db2562ef7a13e5b6c82c55ed6948438abe2
Tamas <jonastamas2@gmail.com>
2020-06-06 17:12:06 +0100

    feat: added useWatchList hook

---
+++ packages/ui/src/components/hooks/useWatchList.js
@@ -0,0 +1,26 @@
+import { useContext, useState, useEffect } from 'react';
+import { firebase } from '@movies/firebase';
+
+import FirebaseAuthContext from '../contexts/FirebaseAuthContext';
+
+const useWatchList = movie => {
+  const { authUser, user } = useContext(FirebaseAuthContext);
+  const [watchlist, setWatchlist] = useState(false);
+  const userRef = firebase.db.ref(`users/${user?.uid}`);
+
+  const toggleMovie = () => {
+    if (user.watchlist && user.watchlist[movie.id]) {
+      userRef.child(`watchlist/${movie.id}`).remove();
+    } else {
+      userRef.child(`watchlist/${movie.id}`).update(movie);
+    }
+  };
+
+  useEffect(() => {
+    setWatchlist(user && user.watchlist && user.watchlist[movie.id]);
+  }, [authUser, user]);
+
+  return { authUser, watchlist, toggleMovie };
+};
+
+export default useWatchList;
